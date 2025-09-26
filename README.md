8baed6e458c89f68fe04ac91365e41c3f86b8bf4
Shreya <shreya.pasupuleti@gmail.com>
2025-09-26 00:00:21 -0700

    Initial commit of starter code of a website template. Home page has made significant progress. Issues added to README.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Issues
+- it works on the web, but when i do local host, the download progress bar doesn't work. 
+- i assume local host just ignores the JS or something. not sure how to overcome it without complicated code. 
+- have to correctly organize everything later
---
+++ docs/scripts.js
@@ -0,0 +1,25 @@
+const bar = document.getElementById('bar');
+const percent = document.getElementById('percent');
+
+const download = () => {
+  //fake stuff
+  let progress = 0;
+  const interval = setInterval(() => {
+    progress++;
+    bar.value = progress; // fill bar
+    percent.textContent = progress + '%';
+    if (progress >= 100){
+      clearInterval(interval);
+    }
+  }, 50);
+  
+  //actual download, with a delay so that in local host 
+  //progress bar starts, then instant download
+  setTimeout(() => {
+  const priv_pol = document.createElement('a'); //create a obj
+    priv_pol.href = 'privacy_policy.pdf'; // define path
+    priv_pol.download = 'privacy_policy.pdf'; // download & click
+    priv_pol.click();
+  }, 500);
+}
+
